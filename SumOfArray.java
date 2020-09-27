054e0d24ce218f9a51cfeb7e6827953e947380b5
Onkar Singtamkar <65884798+onkar1081@users.noreply.github.com>
2020-09-27 15:56:44 +0530

    Add files via upload

---
+++ SumOfArray.java
@@ -0,0 +1,23 @@
+package sumofarray;
+import java.util.Scanner;
+public class SumOfArray {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Ener size of array");
+        int size=ob.nextInt();
+        int[] arr=new int[size];
+        int i,sum=0;
+        System.out.println("Enter array elements.........");
+        for(i=0;i<size;i++)
+        {
+            arr[i]=ob.nextInt();
+        }
+        for(i=0;i<size;i++)
+        {
+            sum=sum+arr[i];
+        }    
+        System.out.println("Addition of array elements is "+sum);
+        
+    }
+    
+}
